a8e38df5c54149efaa6bee19911c38f033fb8e2d
Hassan Tariq <h.tariq@intagleo.co.uk>
2020-10-24 18:55:17 +0500

    header completed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAT5_pnBHZvL_xCIsocjehPePg3CIvjLs",
+    authDomain: "challenge-63fcf.firebaseapp.com",
+    databaseURL: "https://challenge-63fcf.firebaseio.com",
+    projectId: "challenge-63fcf",
+    storageBucket: "challenge-63fcf.appspot.com",
+    messagingSenderId: "449664799460",
+    appId: "1:449664799460:web:2c72d7023d5541e5a2d2f1"
+  };
