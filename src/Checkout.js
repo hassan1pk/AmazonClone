b7f3e37719eeb13c88a76ca4c227bb6775ad405c
Hassan Hashmi <hassan.tariq@gmail.com>
2020-11-01 23:16:16 +0500

    finished removing from basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import CheckoutProduct from "./CheckoutProduct"
+import { useStateValue } from './StateProvider'
 
 const Checkout = () => {
+
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -11,10 +16,20 @@ const Checkout = () => {
                 <div>
                     <h2 className="checkout__title"> Your shopping basket</h2>
 
-                    {/*BasketItem*/}
-                    {/*BasketItem*/}
-                    {/*BasketItem*/}
-                    {/*BasketItem*/}
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
+
+                    {/*CheckoutProduct*/}
+                    {/*CheckoutProduct*/}
+                    {/*CheckoutProduct*/}
+                    {/*CheckoutProduct*/}
                 </div>
             </div>
 
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format"
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 const Subtotal = () => {
+
+    const [{ basket }, dispatch] = useStateValue();
+
+    // use {basket.map(item => item.price).reduce(sumItems)}
+    const sumItems = (total, item) => {
+        return total + item.price;
+    }
+    const sumItems2 = (total, price) => {
+        return total + price;
+    }
+
     return (
         <div className="subtotal">
             <CurrencyFormat
                 renderText={(value) => (
                     <>
                         <p>
-                            {/* part of the home work */}
-                            Subtotal (0 items): <strong>0</strong>
+                            Subtotal ({basket?.length} items): <strong>{value}</strong>
                         </p>
                         <small className="subtotal__gift">
                             <input type="checkbox" /> This order contains a gift
-                    </small>
+                        </small>
                     </>
                 )}
-                decimalSize={2}
-                value={0} //Part of the homework
+                decimalScale={2}
+                value={getBasketTotal(basket)}
+                //value={basket?.map((item) => item.price).reduce(((total, item) => item + total), 0)}
+                //value={basket?.reduce(((total, item) => item.price + total), 0)}
+                //value={basket?.reduce((sumItems), 0)}
+                //value={basket?.map((item) => item.price).reduce((sumItems2), 0)}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
